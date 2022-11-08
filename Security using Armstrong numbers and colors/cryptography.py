f272c215ab56788c2b9b47246b91f42cabfee069
ParthDhore <79781280+ParthDhore@users.noreply.github.com>
2022-11-09 01:17:14 +0530

    Update cryptography.py

--- Security using Armstrong numbers and colors/cryptography.py
+++ Security using Armstrong numbers and colors/cryptography.py
@@ -10,7 +10,13 @@ class Armstrong_Number_Colors:
         self.color_map = self.__generate_color_map__()
 
     def __generate_color__(self):
-        return (27,151,110)
+        #First 6 digits of the key are always unique
+        red_code = (self.key[0] + self.key[1] + self.key[6] + self.key[7]) %256
+        green_code = (self.key[2] + self.key[3] + self.key[8] + self.key[9]) % 256
+        blue_code = (self.key[4] + self.key[5] + self.key[10] + self.key[11])  % 256
+        return (red_code, green_code, blue_code)
+        #return (27,151,110)
+
 
     def __generate_color_map__(self):
         color_map = []
@@ -28,8 +34,12 @@ class Armstrong_Number_Colors:
 
     def __generate_key__(self, user_remark):
         try:
-            # Remove duplicate characters from user_remark
-            user_remark = ''.join(set(user_remark))
+            #Remove duplicate characters from user_remark
+            temp = ''
+            for ch in user_remark:
+                if ch not in temp:
+                    temp += ch
+            user_remark = temp
 
             # Find the sum of ASCII values of the characters of user_remark
             addn = 0
@@ -42,7 +52,7 @@ class Armstrong_Number_Colors:
             # Use the sum as an index to select a Permutation (duplicate selection possible)
             user_permutation = armstrong_permutations[addn % len(armstrong_permutations)]
 
-            # print(user_permutation)
+            print('permutation: ', user_permutation)
 
             # Use the digits of the Permutation to pick the characters of the User_Remark
             picks = []
@@ -53,27 +63,28 @@ class Armstrong_Number_Colors:
             # print(picks)
             picks.extend(user_permutation)
 
-            # Generate a 16 bit key
-            key = picks[:16]
+            # Generate a 12 bit key
+            key = picks[:12]
+            print('key: ', key)
             return key
 
         except IndexError:
-            raise Exception('Too Short Key')
+            raise Exception('Too Short Key, Try with lesser number of duplicate characters')
 
     def encrypt(self, source_file, target_file):
         if not os.path.exists(source_file):
             raise Exception(source_file + ' doesnt exist!')
 
-        #read the source file byte by byte in binary mode
+        #open the source file for reading in binary mode
         src_file_handle = open(source_file, 'rb')
 
-        #write the target file byte by byte
+        #open the target file for writing in binary mode
         trgt_file_handle = open(target_file, 'wb')
 
-        self.index = 0
-        self.map_index = 0
+        self.index = 0 #can be derived from the key
+        self.map_index = 0 #can be derived from the key
         while True:
-            #original data from source file
+            #1 byte of  data from source file
             abyte = src_file_handle.read(1)
             if abyte:
                 abyte = int.from_bytes(abyte, 'big')
@@ -88,10 +99,9 @@ class Armstrong_Number_Colors:
                 trgt_file_handle.write(cbyte)
 
             else:
-                break
+                break #@EOF
         src_file_handle.close()
         trgt_file_handle.close()
-
 
     def __level1__(self, data):
         return data ^ self.key[self.index]
@@ -111,23 +121,23 @@ class Armstrong_Number_Colors:
         if not os.path.exists(source_file):
             raise Exception(source_file + ' doesnt exist!')
 
-        # read the source file byte by byte in binary mode
+        # open the  source file for reading in binary mode
         src_file_handle = open(source_file, 'rb')
 
-        # write the target file byte by byte
+        # open the  target file for writing in binary mode
         trgt_file_handle = open(target_file, 'wb')
 
-        self.index = 0
-        self.map_index = 0
+        self.index = 0 #can be derived from the key
+        self.map_index = 0 #can be derived from the key
         while True:
             # original data from source file
             cbyte = src_file_handle.read(1)
             if cbyte:
                 cbyte = int.from_bytes(cbyte, 'big')
-                # level1 dec. data
+                # level2 dec. data
                 bbyte = self.__dlevel2__(cbyte)
                 self.map_index = (self.map_index + 1) % len(self.color_map)
-                # level2 dec. data
+                # level1 dec. data
                 abyte = self.__level1__(bbyte)
                 self.index = (self.index + 1) % len(self.key)
                 # store in target file
@@ -140,8 +150,39 @@ class Armstrong_Number_Colors:
         trgt_file_handle.close()
 
 def main():
-    anc = Armstrong_Number_Colors('Apple Mango', )
-    anc.encrypt('d:/temp/vikas.jpg', 'd:/temp/file13.jpg')
-    anc.decrypt('d:/temp/file13.jpg', 'd:/temp/vikas_copy.jpg')
+    ch = 0
+    while ch !=3:
+        print('1. Encrypt')
+        print('2. Decrypt')
+        print('3. Exit')
+        print('Enter Choice')
 
+        ch = int(input())
+
+        if ch == 1:
+            try:
+                print('Enter a new Cryptography remark')
+                remark = input()
+                input_file = 'd:/videos/output.avi'
+                output_file = 'd:/temp/e_output.avi'
+                anc = Armstrong_Number_Colors(remark)
+                anc.encrypt(input_file, output_file)
+                print('File encrypted successfully')
+            except:
+                print('File encryption failed')
+        elif ch == 2:
+            try:
+                print('Enter a encryption remark')
+                remark = input()
+                input_file = 'd:/temp/e_output.avi'
+                output_file = 'd:/temp/output.avi'
+                anc = Armstrong_Number_Colors(remark)
+                anc.decrypt(input_file, output_file)
+                print('File decrypted successfully')
+            except:
+                print('File decryption failed')
+        elif ch == 3:
+            break
+        else:
+            print('Wrong Choice')
 main()
